96334348dcb0d3bb7435f972cd8dcfecd35e1364
Dawid Manikowski <ciekacz@gmail.com>
2014-11-09 21:25:09 +0100

    minor update

---
+++ Inc/IBUS.h
@@ -0,0 +1,68 @@
+//--------------------------------------------------------
+//----------DECLARATION OF THE IBUS MESSAGES--------------
+//--------------------------------------------------------
+
+#define GM 0x00 			//   Broadcast, GM
+#define SHD 0x08 			//   SHD
+
+#define CD 0x18 				//   CD Player
+
+#define HKM 0x24 			//   HKM
+#define FUM 0x28 			//   FUM
+
+#define CCM 0x30   
+#define NAV 0x3B 		//NAV, GT  
+#define DIA 0x3F   
+
+#define FBZV 0x40   
+#define GTF 0x43    //Menu Screen,
+#define EWS 0x44    //EWS
+#define CID 0x46    //CID
+#define FMBT 0x47    //FMBT
+
+#define MFL 0x50    //MFL, Multi Functional Steering Wheel Buttons
+#define MML 0x51    //MML
+#define IHK 0x5B    //IHK
+
+#define PDC 0x60    //PDC
+#define CDCD 0x66    //CDCD
+#define RAD 0x68    //RAD, Radio
+#define DSP 0x6A    //DSP
+
+#define RDC 0x70    //RDC
+#define SM 0x72    //SM
+#define SDRS 0x73    //SDRS
+//#define CDCD 0x76    //CDCD
+#define NAVE 0x7F    //NAVE
+
+#define IKE 0x80    //IKE
+
+#define MMR 0x9B    //MMR
+#define CVM 0x9C    //CVM
+
+#define FMID 0xA0    //FMID
+#define ACM 0xA4    //ACM
+#define FHK 0xA7    //FHK
+#define NAVC 0xA8    //NAVC
+#define EHC 0xAC    //EHC
+
+#define SES 0xB0    //SES
+#define TV 0xBB    //TV, NAVJ
+#define LCM 0xBF    //LCM, Light Control Module. GLO
+
+#define MID 0xC0    //MID, Multi Information Display
+#define TEL 0xC8    //Telephone
+
+#define LKM 0xD0    //Navigation Location, LKM
+#define PC 0xD7    //USED BY ME: PC
+#define GPS 0xD8    //USED BY ME: GPS and Powercontrol Module
+#define SMAD 0xDA    //SMAD
+
+#define IRIS 0xE0    //IRIS
+#define OBC 0xE7    //OBC Textbar, ANZV
+#define ISP 0xE8    //ISP
+#define WIPERS 0xED    //Lights, wipers, seat memory, TV
+
+#define BMB 0xF0    //Bordmonitor Buttons
+#define CSU 0xF5   
+#define LOC 0xFF   //Broadcast
